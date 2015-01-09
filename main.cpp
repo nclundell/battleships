9360e2e38d499a19719c94d8782dfdf94a48ab69
nclundell <nclundell@gmail.com>
2015-01-09 17:25:07 -0500

    Added basic files.

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/**
+ *@author Nathan Lundell
+ *@date January 2015
+ *Main runner for Battleships simulator
+ *
+ *
+**/
+
+//Pick Game Type
+    //1. Player vs. Player
+    //2. Player vs. Computer
+    //3. Computer vs. Computer
+
+//Pick number of rounds
+
+//Given game type, pick players
+
+//Run match simulator
+    //runMatch(player1,player2);
+
+//Print Results
---
+++ players/playerDumb/playerDumb.h
@@ -0,0 +1,13 @@
+/**
+ * @author Nathan Lundell
+ * @date January 2015
+ *
+ *
+ */
+
+ #ifndef PLAYERDUMB_H
+ #define PLAYERDUMB_H
+
+ using namespace std;
+
+ #endif

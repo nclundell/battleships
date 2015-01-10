11f6fa28ea15993ad31df45fd1a61e6993586bf1
nclundell <nclundell@gmail.com>
2015-01-09 19:16:46 -0500

    Changed playerList to csv, added ship placement runner files.

--- main.cpp
+++ main.cpp
@@ -15,7 +15,10 @@
 
 //Given game type, pick players
 
+//Run Ship Placement Checker for each custom placer
+    //if(!checkPlacement(player#Placement))
+        //Exit with Error
 //Run match simulator
-    //runMatch(player1,player2);
+    //runMatch(player1Placement, player1Shooter,player2Placement, player2Shooter);
 
 //Print Results
